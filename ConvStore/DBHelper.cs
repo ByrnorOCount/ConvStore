9ef97ef4aa7415ce8497586c02d9c05f297f3a86
Xanthopathy <xanthopathy0@gmail.com>
2025-03-10 10:37:54 +0700

    Add base files

---
+++ ConvStore/DBHelper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+namespace ConvStore
+{
+    class DBHelper : IDisposable
+    {
+        private readonly SqlConnection connection;
+        public SqlConnection Connection
+        {
+            get { return connection; }
+        }
+
+        public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["ConvStoreDB"].ConnectionString;
+        }
+
+        public DBHelper()
+        {
+            connection = new SqlConnection(GetConnectionString());
+        }
+
+        public void OpenConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        public static void TestConnection()
+        {
+            using (SqlConnection conn = new SqlConnection(GetConnectionString()))
+            {
+                conn.Open();
+                Console.WriteLine("Connected successfully!");
+            }
+        }
+
+        public void Dispose()
+        {
+            if (connection != null)
+            {
+                connection.Dispose();
+            }
+        }
+    }
+}
---
+++ ConvStore/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvStore
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            DBHelper.TestConnection(); //remove once connection is confirmed
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ProductManagementApp());
+        }
+    }
+}
